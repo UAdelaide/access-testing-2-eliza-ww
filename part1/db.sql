4d03ae6ad817e9d27e293c2f23f16138b4dbfa0b
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-16 15:05:35 +0000

    [automatic commit] 2025-06-16 15:05:35 UTC 122.148.227.45

--- part1/db.sql
+++ part1/db.sql
@@ -17,7 +17,7 @@ CREATE TABLE message (
     id int NOT NULL AUTO_INCREMENT,
     messenger_id int,
     recipient_id int,
-    message varchar(255),
-    date,
+    contents varchar(255),
+    timestamp date,
     PRIMARY KEY (id)
 );

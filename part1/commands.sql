1a9d10909bbcebcef26dbfc2d4dca5dc6a442308
WDC Student (missing default git user name) <a1924555@student.adelaide.edu.au>
2025-06-19 04:19:36 +0000

    [automatic commit] 2025-06-19 04:19:36 UTC 129.127.249.2

--- part1/commands.sql
+++ part1/commands.sql
@@ -3,6 +3,8 @@ WHERE user_id = {id here}
 INNER JOIN listing ON listing.listing_id = history.listing_id
 INNER JOIN book ON book_isbn = listing.isbn; 
 
+
+
 SELECT * FROM history
 WHERE user_id = 1
 INNER JOIN listing ON history.listing_id = listing.listing_id
